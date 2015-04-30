5bbab94fa437deb4a43c1425426e36130617a13c
Pedro Miguel Henriques Pereira <pmh.pereira@gmail.com>
2015-04-30 20:23:22 +0100

    Added solution template

---
+++ solutions/template.js
@@ -0,0 +1,8 @@
+var init = new Date().getTime();
+
+var result = 0;
+
+console.log(result);
+
+var execution_time = (new Date().getTime() - init) / 1000;
+console.log('executed in ' + execution_time + 's');
